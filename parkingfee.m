df0d0ffeaf2d24b95aa83299bf05a97d37091ccb
Connor Smith <camcon97@gmail.com>
2017-07-14 10:58:25 -0500

    Add files via upload

---
+++ parkingfee.m
@@ -0,0 +1,36 @@
+function [totalFee] = parkingfee(hoursParked)
+        
+    if hoursParked > 0
+        ceil(hoursParked);
+        if hoursParked <= 2
+            totalFee = 4;
+        elseif hoursParked <= 4
+            totalFee = 7;
+        else
+            if hoursParked > 24
+                totalFee = ((floor(hoursParked)*24)+((((rem(hoursParked/24))*24)-4)+7));
+            else
+                totalFee = ((hoursParked-4) + 7);
+            end
+        end
+    else
+       hoursParked = hoursParked*-1;
+       ceil(hoursParked);
+        if hoursParked <= 2
+            totalFee = 4;
+            totalFee = totalFee +36;
+        elseif hoursParked <= 4
+            totalFee = 7;
+            totalFee = totalFee +36;
+        else
+            if hoursParked > 24
+                totalFee = ((floor(hoursParked)*24)+((((rem(hoursParked/24))*24)-4)+7));
+                totalFee=totalFee + 36;
+            else
+                totalFee = ((hoursParked-4) + 7);
+                totalFee = totalFee +36;
+            end
+        end
+    end
+end
+       
